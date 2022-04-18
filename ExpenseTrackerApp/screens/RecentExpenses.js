f469857ff4486d6bcfde83678a2b58e11175a8ea
LeslieChiang <wslchiang@gmail.com>
2022-04-18 19:18:08 +0800

    update to basic RecentExpenses.js

--- ExpenseTrackerApp/screens/RecentExpenses.js
+++ ExpenseTrackerApp/screens/RecentExpenses.js
@@ -1,21 +1,23 @@
-import { useContext } from 'react';
-import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
-import { ExpensesContext } from '../store/expenses-context';
-import { getDateMinusDays } from '../util/date';
+import { useContext } from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import ExpensesContext from "../store/ExpensesContext";
+import { getDateMinusDays } from "../utilities/helpers";
 
 function RecentExpenses() {
-  const expensesCtx = useContext(ExpensesContext);
+  const expenses = useContext(ExpensesContext);
 
-  const recentExpenses = expensesCtx.expenses.filter((expense) => {
-    const today = new Date();
-    const date7DaysAgo = getDateMinusDays(today, 7);
+  console.log(expenses);
 
-    return expense.date >= date7DaysAgo && expense.date <= today;
-  });
+  // const recentExpenses = expenses.filter((expense) => {
+  //   const today = new Date();
+  //   const date7DaysAgo = getDateMinusDays(today, 7);
+
+  //   return expense.date >= date7DaysAgo && expense.date <= today;
+  // });
 
   return (
     <ExpensesOutput
-      expenses={recentExpenses}
+      // expenses={recentExpenses}
       expensesPeriod="Last 7 Days"
       fallbackText="No expenses registered for the last 7 days."
     />
