bbfae5650f2c2377bb8f59a95388430f5fb71782
liewkm <94386756+liewkm@users.noreply.github.com>
2022-04-18 16:32:00 +0800

    Update import in ManageExpense.js

--- ExpenseTrackerApp/screens/ManageExpense.js
+++ ExpenseTrackerApp/screens/ManageExpense.js
@@ -4,10 +4,9 @@
 
 // Leslie: add button for AddExpense ExpenseForm
 
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import ExpensesForm from "../components/ExpensesInput/ExpensesForm";
 import { useContext, useLayoutEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
 import Button from '../components/UI/Button';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
