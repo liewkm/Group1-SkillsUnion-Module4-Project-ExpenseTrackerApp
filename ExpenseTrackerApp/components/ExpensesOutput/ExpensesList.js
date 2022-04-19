d976e01401c371776c4233fda369de717f6e697d
Keith <keithtanweikeat@gmail.com>
2022-04-19 16:02:32 +0800

    components update

--- ExpenseTrackerApp/components/ExpensesOutput/ExpensesList.js
+++ ExpenseTrackerApp/components/ExpensesOutput/ExpensesList.js
@@ -2,12 +2,32 @@
   Expenses list
 ----*/
 
-import { Text } from 'react-native'
+// import { Text } from 'react-native'
 
-function ExpensesList() {
-  return (
-    <Text>ExpensesList</Text>
-  )
+// function ExpensesList() {
+//   return (
+//     <Text>ExpensesList</Text>
+//   )
+// }
+
+// export default ExpensesList
+
+import { FlatList } from 'react-native';
+
+import ExpenseItem from './ExpenseItem';
+
+function renderExpenseItem(itemData) {
+  return <ExpenseItem {...itemData.item} />;
 }
 
-export default ExpensesList
+function ExpensesList({ expenses }) {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpenseItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+}
+
+export default ExpensesList;
