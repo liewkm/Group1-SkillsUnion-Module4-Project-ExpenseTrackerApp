3dd83a55eb8bcbff8b68b91969b63d68babe3a86
CP Wong <wongchenpang@gmail.com>
2022-04-21 09:30:39 +0800

    Add dropdown picker + replace useLayoutEffect

--- ExpenseTrackerApp/components/ExpensesOutput/ExpensesOutput.js
+++ ExpenseTrackerApp/components/ExpensesOutput/ExpensesOutput.js
@@ -12,6 +12,8 @@
 
 // export default ExpensesOutput
 
+import { useEffect, useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { GlobalColors } from '../../utilities/colors';
@@ -19,15 +21,45 @@ import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
 
 function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
+  const [pickerValue, setPickerValue] = useState(null)
+  const [pickerItems, setPickerItems] = useState([])
+
   let content = <Text style={styles.infoText}>{fallbackText}</Text>;
 
   if (expenses.length > 0) {
     content = <ExpensesList expenses={expenses} />;
   }
+  
+  // Filters out category items into an array for dropdown menu
+  
+  useEffect(() => {
+    let array = [];
+    expenses.forEach((item) => {
+      array.push(item.category);
+    });
+    const unique = [...new Set(array)];
+    setPickerItems(unique)
+  }, [expenses.length])
+
+  // Updates selected category item from dropdown menu
+  
+  useEffect(() => {
+    console.log('ExpensesOutput: pickerValue', pickerValue);
+  }, [pickerValue])
 
   return (
     <View style={styles.container}>
       <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <Picker
+        selectedValue={pickerValue}
+        style={{color: 'white'}}
+        dropdownIconColor={'white'}
+        onValueChange={(value, index) => setPickerValue(value)}
+      >
+        {pickerItems.map((item, index) => {
+          return <Picker.Item key={index} label={item} value={item} /> 
+        })}
+      </Picker>      
       {content}
     </View>
   );
